4f107acc4a781cba1b6f414fec9ff3e75aaa8cb7
pengbeirong <pbeirong@sina.com>
2020-03-12 21:11:17 +0800

    *add 增加一个tabbar组件

---
+++ src/components/TabBarView.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="view-box-container">
+        <view-box ref="viewBox">
+            <router-view></router-view>
+        </view-box>
+        <tabbar>
+            <tabbar-item
+                :selected="routeName === 'Home'"
+                icon-class="tab-bar-icon-class"
+                @click.native="toReturn('Home')"
+            >
+                <span slot="icon" class="iconfont icon-home"></span>
+                <span slot="label">首页</span>
+            </tabbar-item>
+            <tabbar-item
+                :selected="routeName === 'About'"
+                icon-class="tab-bar-icon-class"
+                @click.native="toReturn('About')"
+            >
+                <span slot="icon" class="iconfont icon-mine"></span>
+                <span slot="label">关于</span>
+            </tabbar-item>
+        </tabbar>
+    </div>
+</template>
+
+<script>
+import { ViewBox, Tabbar, TabbarItem } from "vux";
+import { getTokenId } from "src/api/token";
+
+export default {
+    name: "tabBarView",
+    components: {
+        ViewBox,
+        Tabbar,
+        TabbarItem
+    },
+    data() {
+        return {
+            routeName: "Home"
+        };
+    },
+    mounted() {
+        this.$nextTick(function() {
+            this.routeName = this.$route.name;
+        });
+    },
+    methods: {
+        toReturn(name) {
+            this.$router.replace({
+                name: name
+            })
+        }
+    }
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,30 @@
+import TabBarView from 'src/components/TabBarView'
 import Home from 'src/pages/home'
 import About from 'src/pages/about'
 
 export default [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
-        meta: { needLogin: false }
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: About,
-        meta: { needLogin: false }
+        name: 'TabBarView',
+        component: TabBarView,
+        redirect: '/home',
+        children: [
+            {
+                path: '/home',
+                name: 'Home',
+                component: Home,
+                meta: {
+                    needLogin: false
+                }
+            },
+            {
+                path: '/about',
+                name: 'About',
+                component: About,
+                meta: {
+                    needLogin: false
+                }
+            }
+        ]
     }
 ]
